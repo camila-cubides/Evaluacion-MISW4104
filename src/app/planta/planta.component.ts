7daae560836169292026a0fda1dc88220fdfb298
María Camila Cubides Martín <mc.cubides@uniandes.edu.co>
2025-04-25 13:36:38 -0500

    Añadir estructura modulo planta

---
+++ src/app/planta/planta.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planta',
+  templateUrl: './planta.component.html',
+  styleUrls: ['./planta.component.css'],
+  standalone: false,
+})
+export class PlantaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
